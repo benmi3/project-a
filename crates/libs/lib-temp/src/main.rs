d5f66ecdcbfeac7b7fd2c4c4b50761c1997c27c2
Benjamin Jørgensen <69441266+benmi3@users.noreply.github.com>
2024-04-11 12:32:17 +0900

    Update main.rs

--- crates/libs/lib-temp/src/main.rs
+++ crates/libs/lib-temp/src/main.rs
@@ -1,15 +1,9 @@
 #[derive(askama::Template)]
 #[template(path = "index.html")]
 struct IndexTemplate {
-    todos: Vec<TodoTemplate>,
+    page_title: String,
 }
 
-async fn index(
-    axum::extract::State(pool): axum::extract::State<db::DBPool>,
-) -> Result<IndexTemplate, (axum::http::StatusCode, String)> {
-    sqlx::query_as!(TodoTemplate, "SELECT * FROM todo")
-        .fetch_all(&pool)
-        .await
-        .map(|todos| IndexTemplate { todos })
-        .map_err(db::map_db_err)
+async fn index() -> Result<IndexTemplate, (axum::http::StatusCode, String)> {
+    IndexTemplate
 }
